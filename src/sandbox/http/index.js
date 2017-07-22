56a16fc8e47e551bf7a206e6d0256a3381c65584
Brian LeRoux <b@brian.io>
2017-07-22 16:42:07 -0700

    updates sandbox to inc the fresh cookie sigs

--- src/sandbox/http/index.js
+++ src/sandbox/http/index.js
@@ -13,6 +13,7 @@ var local = require('./_local')
 var app = express()
 app.use(body.json())
 app.use(body.urlencoded({extended:false}))
+app.disable('x-powered-by')
 
 app.start = function _start(callback) {
   registerRoutes('json')
@@ -47,13 +48,16 @@ function registerRoutes(type) {
       var route = r[1]
       var path = route === '/'? '-index' : route.replace(/\//g, '-').replace(/:/g, '000')
       var funct = require(join(process.cwd(), 'src', type, `${verb}${path}`)).handler
+      var left = `@${chalk.dim(type)}${chalk.dim(verb === 'get'? ' get' : verb)}`
+      var right = `${padend(chalk.cyan(route), 44, '.')} ${verb}${path}`
 
-      console.log(`@${chalk.dim(type)}${chalk.dim(verb === 'get'? ' get' : verb)} ${padend(chalk.cyan(route), 44, '.')} ${verb}${path}`)
+      console.log(`${left} ${right}`)
 
       // actually reg the route
       app[verb](route, function _http(req, res) {
         // api gateway 'Cookie' from express 'cookie'
         req.headers.Cookie = req.headers.cookie
+        // 
         // run the lambda sig locally
         local(funct, {
           method: verb,
@@ -64,15 +68,31 @@ function registerRoutes(type) {
           params: req.params,
         },
         function _local(err, result) {
-          // res.setHeader('Set-Cookie', result.cookie)
-          var arcCookie = cookie.parse(result.cookie)
-          res.cookie('_idx', arcCookie._idx, {maxAge:arcCookie['Max-Age'], httpOnly:true})
-          if (type === 'json' && result.json) {
-            res.json(err? err : result.json)
+          if (err && err.startsWith('/')) {
+            res.redirect(err)
           }
-          else if (type === 'html' && result.html) {
+          else if (err && type === 'json') {
+            var v = JSON.parse(err)
+            res.status(v.statusCode).json(v.json)
+          }
+          else if (err && type === 'html') {
+            var v = JSON.parse(err)
             res.set('Content-Type', 'text/html');
-            res.end(err? err : result.html)
+            res.status(v.statusCode).end(v.html)
+          }
+          else if (err) {
+            res.status(500).end(err) 
+          }
+          else {
+            var arcCookie = cookie.parse(result.cookie)
+            res.cookie('_idx', arcCookie._idx, {maxAge:arcCookie['Max-Age'], httpOnly:true})
+            if (type === 'json' && result.json) {
+              res.json(err? err : result.json)
+            }
+            else if (type === 'html' && result.html) {
+              res.set('Content-Type', 'text/html');
+              res.end(err? err : result.html)
+            }  
           }
         })
       })
--- test/create/_mock/.arc
+++ test/create/_mock/.arc
@@ -1,14 +1,6 @@
 @app
 testapp
 
-@html
-get /
-#post /hi
-#post /hi/nested
-#get /hi/nested/:paramID
-#post /hi/nested/:paramID
-
-@json
-get /nested/page
-#post /nested/:paramID
-#get /nested/:paramID/foo/:bar
+@events
+test-event
+test-event-two
