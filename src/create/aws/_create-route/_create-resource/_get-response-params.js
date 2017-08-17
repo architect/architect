ae3699c8e3bbd78dfbbca0d02542494c9a7c61fa
Brian LeRoux <b@brian.io>
2017-08-16 21:23:26 -0700

    fixes #15; adds charset to Content-Type: text/html response

--- src/create/aws/_create-route/_create-resource/_get-response-params.js
+++ src/create/aws/_create-route/_create-resource/_get-response-params.js
@@ -1,7 +1,7 @@
 module.exports = function _getReponseParams(type) {
   if (type === 'html') {
     return {
-      'method.response.header.Content-Type': "'text/html'",
+      'method.response.header.Content-Type': "'text/html; charset=utf-8'",
       'method.response.header.Set-Cookie': 'integration.response.body.cookie',
       'method.response.header.Location': 'integration.response.body.errorMessage',
     }
