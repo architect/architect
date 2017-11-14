20a0e2af01f5cdd83df15f9ca2f921c5e12b3650
Brian LeRoux <b@brian.io>
2017-11-13 20:24:20 -0800

    aggressive overwrite

--- src/sandbox/http/_copy-shared.js
+++ src/sandbox/http/_copy-shared.js
@@ -12,6 +12,7 @@ module.exports = function _shared(params, callback) {
     var paths = glob.sync('src/@(html|json|events|scheduled|tables|slack)/*') 
     paths.forEach(pathToCode=> {
       var dest = path.join(process.cwd(), pathToCode, 'node_modules', '@architect', 'shared')
+      fs.removeSync(dest)
       fs.copySync(src, dest, {overwrite:true})
     })
   }
