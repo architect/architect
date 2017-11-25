91d811fe95d44fd17b539f72e6025bb50822cf17
Brian LeRoux <b@brian.io>
2017-11-26 08:47:30 +1100

    adds a quick tests

---
+++ test/create/05-table-index-test.js
@@ -0,0 +1,73 @@
+var nukeLambdas = require('./_nuke-lambdas')
+var nukeTables = require('./_nuke-tables')
+var path = require('path')
+var rm = require('rimraf').sync
+var mkdir = require('mkdirp').sync
+var cp = require('cp').sync
+var test = require('tape')
+var run = require('../../src/create')
+
+test('env', t=> {
+  t.plan(1)
+  t.ok(run, 'run exists')
+})
+
+test('setup', t=> {
+  t.plan(1)
+  mkdir('test/create/_mock')
+  cp('test/create/05-tables-mock.arc', 'test/create/_mock/.arc')
+  process.chdir('test/create/_mock')
+  t.ok(true, 'created test/create/_mock/.arc')
+  console.log(process.cwd())
+})
+
+test('plan', t=> {
+  t.plan(1)
+  run({
+    arcFile: path.join(process.cwd(), '.arc'),
+    execute: false
+  },
+  function _ran(err, plans) {
+    if (err) {
+      t.fail(err)
+    }
+    else {
+      t.ok(plans, 'got plans')
+      console.log(JSON.stringify(plans, null, 2))
+    }
+  })
+})
+
+test('exec', t=> {
+  t.plan(1)
+  run({
+    arcFile: path.join(process.cwd(), '.arc'),
+    execute: true
+  },
+  function _ran(err) {
+    if (err) {
+      t.fail(err)
+    }
+    else {
+      t.ok(true, 'got plans')
+    }
+  })
+})
+/*
+test('cleanup dynamo tables', t=> {
+  t.plan(1)
+  nukeTables([
+    'testapp-staging-ppl',
+    'testapp-production-ppl',
+    'testapp-staging-posts',
+    'testapp-production-posts',
+  ],
+  function _nuke(err) {
+    if (err) {
+      t.fail(err)
+    }
+    else {
+      t.ok(true, 'lambdas nuked')
+    }
+  })
+})*/
