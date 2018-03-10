cd37bf3d0a7f17e6071afa09e70475b8bc89242f
Brian LeRoux <b@brian.io>
2018-03-10 13:54:59 -0800

    makes default generated POST lambda use req._url helper

--- src/create/templates/html-lambda/post.js
+++ src/create/templates/html-lambda/post.js
@@ -2,7 +2,9 @@ var arc = require('@architect/functions')
 
 function route(req, res) {
   console.log(JSON.stringify(req, null, 2))
-  res({location:`/`})
+  res({
+    location: req._url('/')
+  })
 }
 
 exports.handler = arc.html.post(route)
