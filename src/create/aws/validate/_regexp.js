431989914253b9a9c1037b7147cdde63c59179cf
Brian LeRoux <b@brian.io>
2018-05-05 16:21:07 -0700

    fix #44

--- src/create/aws/validate/_regexp.js
+++ src/create/aws/validate/_regexp.js
@@ -4,6 +4,7 @@ module.exports = {
   eventname: /^[a-z][a-z|\-|0-9]+/,    // lowercase alphanumeric dasherized (must start with a letter)
   slackname: /^[a-z][a-z|\-|0-9]+/,    // lowercase alphanumeric dasherized (must start with a letter)
   schedulename: /^[a-z][a-z|\-|0-9]+/, // lowercase alphanumeric dasherized (must start with a letter)
+  tablename: /^[a-z][a-z|\-|0-9]+/,    // lowercase alphanumeric dasherized (must start with a letter)
   rate: /rate\((.+)\)/,                // "rate(1 minute)".match(regexp.rate) returns ['rate(1 minutes)', '1 minutes']
   cron: /cron\((.+)\)/,                // "cron(* * ? * * *)".match(regexp.cron) returns ['cron(* * ? * * *)', '* * ? * * *']
   rateExp: /^\d+ (minute(s)?|hour(s)?|day(s)?)/,
