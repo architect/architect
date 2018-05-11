886f219ed4723135c8f985078f3ecf5bd9ba5fcc
brianleroux <b@brian.io>
2018-05-10 23:30:47 -0700

    fix merge conflict
    still learning the github desktop app!

--- src/deploy/lambda/03-copy-shared.js
+++ src/deploy/lambda/03-copy-shared.js
@@ -8,33 +8,11 @@ let cp = require('cpr')
 module.exports = function _shared(params, callback) {
 
   var {pathToCode} = params
-<<<<<<< HEAD
-
   var src = path.join(process.cwd(), 'src', 'shared')
   var dest = path.join(process.cwd(), pathToCode, 'node_modules', '@architect', 'shared')
   var arcFileSrc = path.join(process.cwd(), '.arc')
   var arcFileDest = path.join(dest, '.arc')
-=======
-  var destDir = path.join(process.cwd(), pathToCode, 'node_modules', '@architect', 'shared')
-  var arcFileSrc = path.join(process.cwd(), '.arc')
-  var arcFileDest = path.join(destDir, '.arc')
-  var src = path.join(process.cwd(), 'src', 'shared')
-  var files = glob.sync(src + '/**/*', {dot:true})
 
-  // mkdir the architect/shared dir if it does not exist
-  mkdir(destDir)
-
-  // walk the files and dirs
-  files.forEach(f=> {
-    var current = f.replace(src, destDir)
-    if (fs.statSync(f).isDirectory()) {
-      mkdir(current)
-    }
-    else {
-      fs.copyFileSync(f, current)
-    }
-  })
->>>>>>> 893f9d6d0a100e68c37cb14934ead689db779ddd
 
   series([
     function copyShared(callback) {
