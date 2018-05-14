6c733ba47f6fc8c6e191382036b9a320a7cb8a4b
Brian LeRoux <b@brian.io>
2018-05-14 13:19:21 -0700

    fix: lint

--- src/env/_put.js
+++ src/env/_put.js
@@ -1,11 +1,11 @@
-let aws = require('aws-sdk')
+//let aws = require('aws-sdk')
 
 module.exports = function _put(params, callback) {
-   
+
   /*
   let ssm = new aws.SSM
   ssm.putParameter({
-    Name: 'STRING_VALUE', 
+    Name: 'STRING_VALUE',
     Value: 'STRING_VALUE',
     Type: 'SecureString',
     Overwrite: true
