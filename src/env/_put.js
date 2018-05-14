09b0befb5d8ae338faad9a8ab212166ef99c0eb3
Brian LeRoux <b@brian.io>
2018-05-14 10:56:53 -0700

    fix: missing env

--- src/env/_put.js
+++ src/env/_put.js
@@ -1,12 +1,15 @@
 let aws = require('aws-sdk')
-let ssm = new aws.SSM
 
-ssm.putParameter({
-  Name: 'STRING_VALUE', /* required */
-  Value: 'STRING_VALUE', /* required */
-  Type: 'SecureString',
-  Overwrite: true
-}, function(err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else     console.log(data);           // successful response
-})
+module.exports = function _put(params, callback) {
+   
+  /*
+  let ssm = new aws.SSM
+  ssm.putParameter({
+    Name: 'STRING_VALUE', 
+    Value: 'STRING_VALUE',
+    Type: 'SecureString',
+    Overwrite: true
+  }, callback)
+  */
+  callback(params)
+}
