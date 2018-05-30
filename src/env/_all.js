b03d069d70c535fb33989b1ed135d247fedf07ea
Brian LeRoux <b@brian.io>
2018-05-29 21:10:03 -0700

    fix for large SSM results requiring pagination

--- src/env/_all.js
+++ src/env/_all.js
@@ -1,27 +1,47 @@
 let aws = require('aws-sdk')
+let ssm = new aws.SSM
+let result = []
 
-module.exports = function _all(appname, callback) {
-  let ssm = new aws.SSM
+function getSome(appname, NextToken, callback) {
+  // base query to ssm
   let query = {
     Path: `/${appname}`,
     Recursive: true,
+    MaxResults: 10, 
     WithDecryption: true
   }
+  // check if we're paginating
+  if (NextToken) {
+    query.NextToken = NextToken
+  }
+  // performs the query
   ssm.getParametersByPath(query, function _query(err, data) {
     if (err) {
       callback(err)
     }
     else {
-      var result = data.Parameters.map(function(param) {
+      // tidy up the response
+      result = result.concat(data.Parameters.map(function(param) {
         let bits = param.Name.split('/')
         return {
-          app: bits[0],
+          app: appname,
           env: bits[2],
           name: bits[3],
           value: param.Value,
         }
-      })
-      callback(null, result)
+      }))
+      // check for more data and, if so, recurse
+      if (data.NextToken) {
+        getSome(appname, data.NextToken, callback) 
+      }
+      else {
+        // otherwise callback
+        callback(null, result)
+      }
     }
   })
+}
+
+module.exports = function _all(appname, callback) {
+  getSome(appname, false, callback)
 }
