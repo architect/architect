f7f2f8dcd8dddc24ec72954b3871540647d79493
Brian LeRoux <b@brian.io>
2018-06-02 10:56:12 -0700

    npx hydrate now installs modules for any package.json files found in src/shared

--- src/hydrate/index.js
+++ src/hydrate/index.js
@@ -15,34 +15,48 @@ module.exports = {
 }
 
 /**
- * runs either install or update function on all lambdas
+ * runs either install or update function on all lambdas and shared modules
  */
 function _initDeps(installing, callback) {
   let exec = installing? _install : _update
   let banner = chalk.dim.cyan(installing? 'Installing' : 'Updating')
   let start = Date.now()
-  let pattern = 'src/@(html|css|js|text|xml|json|events|scheduled|tables|slack)/*'
-  glob(pattern, function _glob(err, results) {
-    if (err) callback(err)
-    else {
-      let total = results.length*2
-      let width = 30
-      let complete = '\u001b[46m \u001b[0m'
-      let incomplete = '\u001b[40m \u001b[0m'
-      let clear = true
-      bar = new progress(`${banner} :bar`, {total, width, complete, incomplete, clear})
-      parallel(results.map(pathToCode=> {
-        return function _install(callback) {
-          exec(pathToCode, callback)
-        }
-      }),
-      function(err) {
-        if (err) throw err
-        let ts = Date.now() - start
-        console.log(chalk.grey(chalk.cyan('✓'), `Success ${ts}ms`))
-        callback()
-      })
+  // do two glob calls; one for src/lambdas and one for src/shared
+  parallel({
+    lambdas(callback) {
+      // all the lambda folders
+      let pattern = 'src/@(html|css|js|text|xml|json|events|scheduled|tables|slack)/*'
+      glob(pattern, callback) 
+    },
+    shared(callback) {
+      // any folders under shared with package.json
+      let pattern = 'src/shared/**/*/package.json'
+      glob(pattern, callback) 
     }
+  }, 
+  function _glob(err, both) {
+    // clean up shared removing 'package.json' and paths that include node_modules
+    let shared = both.shared.map(p=> p.replace('package.json', '')).filter(e=> !e.includes('node_modules'))
+    let results = [].concat(both.lambdas).concat(shared)
+    // two ticks per install/update
+    let total = results.length*2
+    let width = 30
+    let complete = '\u001b[46m \u001b[0m'
+    let incomplete = '\u001b[40m \u001b[0m'
+    let clear = true
+    bar = new progress(`${banner} :bar`, {total, width, complete, incomplete, clear})
+    // exec the fn in parallel across all folders
+    parallel(results.map(pathToCode=> {
+      return function _install(callback) {
+        exec(pathToCode, callback)
+      }
+    }),
+    function(err) {
+      if (err) throw err
+      let ts = Date.now() - start
+      console.log(chalk.grey(chalk.cyan('✓'), `Success ${ts}ms`))
+      callback()
+    })
   })
 }
 
