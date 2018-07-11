763d7983176ea182cb70390531bb7fa75b93c84f
Brian LeRoux <b@brian.io>
2018-07-11 11:58:32 -0700

    change NIX_CONTAINER to PARALLEL_DEPLOYS_PER_SECOND

--- src/deploy/_chunk.js
+++ src/deploy/_chunk.js
@@ -1,7 +1,7 @@
 module.exports = function _chunk(arr, len=12) {
 
-
-  if (process.env.NIX_CONTAINER) len = 1
+  if (process.env.PARALLEL_DEPLOYS_PER_SECOND) 
+    len = Number(process.env.PARALLEL_DEPLOYS_PER_SECOND)
 
   let chunks = []
   let index = 0
