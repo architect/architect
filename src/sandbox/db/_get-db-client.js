e5758f0d9aaffb2899e3c2df63a52dd7244f35dd
Brian LeRoux <b@brian.io>
2018-08-11 12:47:17 -0700

    hardcode sandbox db to us-west-2

--- src/sandbox/db/_get-db-client.js
+++ src/sandbox/db/_get-db-client.js
@@ -1,5 +1,6 @@
 var AWS = require('aws-sdk')
 var endpoint = new AWS.Endpoint('http://localhost:5000')
-var dynamo = new AWS.DynamoDB({endpoint})
+let region= 'us-west-2'
+var dynamo = new AWS.DynamoDB({endpoint, region})
 
 module.exports = dynamo
--- src/sandbox/db/_get-doc-client.js
+++
@@ -1,5 +0,0 @@
-var AWS = require('aws-sdk')
-var endpoint = new AWS.Endpoint('http://localhost:5000')
-var client = new AWS.DynamoDB.DocumentClient({endpoint})
-
-module.exports = client
