6845ab4629fcf9b8e977082a058744e933b93f87
Brian LeRoux <b@brian.io>
2018-11-12 12:38:09 -0800

    add some notes about the new /public behavior locally

--- src/sandbox/http/public-middleware.js
+++ src/sandbox/http/public-middleware.js
@@ -3,8 +3,12 @@ let send = require('send')
 let path = require('path')
 let exists = require('path-exists').sync
 
+/**
+ * serves static assets found in /public
+ * - if /public/index.html exists it will serve it as / (even if `get /` http lambda is defined)
+ */ 
 module.exports = function _public(req, res, next) {
-  
+
   // assume if public/index.html exists we want to treat that as the app apex
   let legacy = exists(path.join(process.cwd(), 'public', 'index.html'))
   if (legacy) {
@@ -15,7 +19,7 @@ module.exports = function _public(req, res, next) {
   let notFound = !exists(path.join(process.cwd(), 'public', pathToFile))
   if (notFound) {
     next()
-  } 
+  }
   else {
     function error(err) {
       res.statusCode = err.status || 500
