92eaa4cc1a74ea178d31db2fccfb722eebe71ed3
Brian LeRoux <b@brian.io>
2018-11-12 17:18:29 -0800

    fix lint

--- src/sandbox/http/invoke-lambda.js
+++ src/sandbox/http/invoke-lambda.js
@@ -1,3 +1,4 @@
+let parse = require('@architect/parser')
 let {spawn} = require('child_process')
 let path = require('path')
 let exists = require('path-exists').sync
@@ -16,6 +17,8 @@ module.exports = function local(cwd, event, callback) {
     PYTHONUNBUFFERED: true
   })
 
+  let timedout = false
+  let timeout = 3*1000 // 3 seconds in milliseconds
   let runtime = path.join(__dirname, '/runtimes/node.js')
   let command = 'node'
   let script = fs.readFileSync(runtime).toString()
@@ -25,16 +28,32 @@ module.exports = function local(cwd, event, callback) {
   // select the command based on the current function runtime (look at .arc then at .arc-config for override)
   let config = exists(path.join(cwd, '.arc-config'))
   if (config) {
-    runtime = path.join(__dirname, '/runtimes/python.py')
-    command = 'python3'
-    script = `"${fs.readFileSync(runtime).toString()}"`
-    options = ['-c', script]
+    let raw = fs.readFileSync(path.join(cwd, '.arc-config')).toString()
+    let arc = parse(raw)
+    let runtime = arc.aws.find(tuple=> tuple.includes('runtime'))
+    if (runtime && runtime[1] === 'python3.6') {
+      runtime = path.join(__dirname, '/runtimes/python.py')
+      command = 'python3'
+      script = `"${fs.readFileSync(runtime).toString()}"`
+      options = ['-c', script]
+    }
+
+    let ttl = arc.aws.find(tuple=> tuple.includes('timeout'))
+    if (ttl && ttl[1] > 3 && ttl[1] < 900) {
+      timeout = ttl[1]*1000 // ttl seconds in milliseconds
+    }
   }
 
   // run the show
   let child = spawn(command, options, args)
   let stdout = ''
   let stderr = ''
+
+  // bake a timeout
+  setTimeout(function() {
+    timedout = true
+    child.kill()
+  }, timeout)
 
   child.stdout.on('data', data=> {
     // always capture data piped to stdout
@@ -50,7 +69,14 @@ module.exports = function local(cwd, event, callback) {
   })
 
   child.on('close', function done(code) {
-    if (code === 0) {
+    if (timedout) {
+      callback(null, {
+        type: 'text/html',
+        body: `<h1>Timeout Error</h1>
+        <p>Lambda <code>${cwd}</code> timed out after <b>${timeout/1000} seconds</b></p>`
+      })
+    }
+    else if (code === 0) {
       // extract the __ARC__ line
       let command = line=> line.startsWith('__ARC__')
       let result = stdout.split('\n').find(command).replace('__ARC__', '')
