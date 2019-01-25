7434deeb763c6c8457e9dec9a04a145417153eac
Ryan Block <rblock@gmail.com>
2019-01-25 11:15:37 -0800

    Fixes #276

--- src/create/aws/report/render.js
+++ src/create/aws/report/render.js
@@ -2,19 +2,25 @@ let chalk = require('chalk')
 let res = s=> console.log(chalk.cyan.underline(s))
 
 module.exports = function render(arc, result) {
-
   let region = process.env.AWS_REGION
   let appname = arc.app[0]
 
-  let httpStaging = result.http.find(api=> api.name === `${appname}-staging`).id
-  let httpProduction = result.http.find(api=> api.name === `${appname}-production`).id
+  let restApis = arc.hasOwnProperty('http')
+  let websocketApis = arc.hasOwnProperty('ws')
 
-  let wsStaging = result.ws.find(api=> api.Name === `${appname}-ws-staging`).ApiEndpoint
-  let wsProduction = result.ws.find(api=> api.Name === `${appname}-ws-production`).ApiEndpoint
+  if (restApis) {
+    let httpStaging = result.http.find(api=> api.name === `${appname}-staging`).id
+    let httpProduction = result.http.find(api=> api.name === `${appname}-production`).id
 
-  res(`https://${httpStaging}.execute-api.${region}.amazonaws.com/staging`)
-  res(`https://${httpProduction}.execute-api.${region}.amazonaws.com/production`)
+    res(`https://${httpStaging}.execute-api.${region}.amazonaws.com/staging`)
+    res(`https://${httpProduction}.execute-api.${region}.amazonaws.com/production`)
+  }
 
-  res(`${wsStaging}/staging`)
-  res(`${wsProduction}/production`)
+  if (websocketApis) {
+    let wsStaging = result.ws.find(api=> api.Name === `${appname}-ws-staging`).ApiEndpoint
+    let wsProduction = result.ws.find(api=> api.Name === `${appname}-ws-production`).ApiEndpoint
+
+    res(`${wsStaging}/staging`)
+    res(`${wsProduction}/production`)
+  }
 }
