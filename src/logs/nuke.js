61f8760876a886f3fd18285c5fc192c0f23e5788
Mikeal Rogers <mikeal.rogers@gmail.com>
2019-02-22 08:59:08 -0800

    fix: paginate logs when nuking
    fixes #311

--- src/logs/nuke.js
+++ src/logs/nuke.js
@@ -1,31 +1,30 @@
-let aws = require('aws-sdk')
-let parallel = require('run-parallel')
-let waterfall = require('run-waterfall')
-
-module.exports = function nuke(name, callback) {
-
-  let region = process.env.AWS_REGION
-  let cloud = new aws.CloudWatchLogs({region})
-
-  waterfall([
-    function describeLogStreams(callback) {
-      cloud.describeLogStreams({
-        logGroupName: name
-      }, callback)
-    },
-    function getLogEvents(result, callback) {
-      parallel(result.logStreams.map(logStream=> {
-        return function getOneLogEventStream(callback) {
-          cloud.deleteLogStream({
-            logGroupName: name,
-            logStreamName: logStream.logStreamName,
-          }, callback)
-        }
-      }), callback)
-    },
-  ],
-  function done(err) {
-    if (err) callback(err)
-    else callback()
-  })
+let aws = require('aws-sdk')                                                                                                                          │··········
+let parallel = require('run-parallel')                                                                                                                │··········
+                                                                                                                                                      │··········
+module.exports = function nuke(name, callback) {                                                                                                      │··········
+                                                                                                                                                      │··········
+  let region = process.env.AWS_REGION                                                                                                                 │··········
+  let cloud = new aws.CloudWatchLogs({region})                                                                                                        │··········
+                                                                                                                                                      │··········
+  let deleteLogs = streams => parallel(streams.map(log => cb => {                                                                                     │··········
+    cloud.deleteLogStream({                                                                                                                           │··········
+      logGroupName: name,                                                                                                                             │··········
+      logStreamName: log.logStreamName,                                                                                                               │··········
+    }, cb)                                                                                                                                            │··········
+  }))                                                                                                                                                 │··········
+                                                                                                                                                      │··········
+  let count = 0                                                                                                                                       │··········
+                                                                                                                                                      │··········
+  let run = () => {                                                                                                                                   │··········
+    cloud.describeLogStreams({logGroupName: name}, (err, streams) => {                                                                                │··········
+      streams = streams.logStreams                                                                                                                    │··········
+      if (!streams.length) return callback(null, count)                                                                                               │··········
+      count += streams.length                                                                                                                         │··········
+      deleteLogs(streams, (err) => {                                                                                                                  │··········
+        if (err) return callback(err)                                                                                                                 │··········
+        else run()                                                                                                                                    │··········
+      })                                                                                                                                              │··········
+    })                                                                                                                                                │··········
+  }                                                                                                                                                   │··········
+  run()                                                                                                                                               │··········
 }
