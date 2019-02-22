a5f1d2b93ae4d2c84d4f995a28342e6431d17f3e
Mikeal Rogers <mikeal.rogers@gmail.com>
2019-02-22 09:11:41 -0800

    fix: copy/paste fail from working terminal

--- src/logs/nuke.js
+++ src/logs/nuke.js
@@ -1,30 +1,30 @@
-let aws = require('aws-sdk')                                                                                                                          │··········
-let parallel = require('run-parallel')                                                                                                                │··········
-                                                                                                                                                      │··········
-module.exports = function nuke(name, callback) {                                                                                                      │··········
-                                                                                                                                                      │··········
-  let region = process.env.AWS_REGION                                                                                                                 │··········
-  let cloud = new aws.CloudWatchLogs({region})                                                                                                        │··········
-                                                                                                                                                      │··········
-  let deleteLogs = streams => parallel(streams.map(log => cb => {                                                                                     │··········
-    cloud.deleteLogStream({                                                                                                                           │··········
-      logGroupName: name,                                                                                                                             │··········
-      logStreamName: log.logStreamName,                                                                                                               │··········
-    }, cb)                                                                                                                                            │··········
-  }))                                                                                                                                                 │··········
-                                                                                                                                                      │··········
-  let count = 0                                                                                                                                       │··········
-                                                                                                                                                      │··········
-  let run = () => {                                                                                                                                   │··········
-    cloud.describeLogStreams({logGroupName: name}, (err, streams) => {                                                                                │··········
-      streams = streams.logStreams                                                                                                                    │··········
-      if (!streams.length) return callback(null, count)                                                                                               │··········
-      count += streams.length                                                                                                                         │··········
-      deleteLogs(streams, (err) => {                                                                                                                  │··········
-        if (err) return callback(err)                                                                                                                 │··········
-        else run()                                                                                                                                    │··········
-      })                                                                                                                                              │··········
-    })                                                                                                                                                │··········
-  }                                                                                                                                                   │··········
-  run()                                                                                                                                               │··········
+let aws = require('aws-sdk')
+let parallel = require('run-parallel')
+
+module.exports = function nuke(name, callback) {
+
+  let region = process.env.AWS_REGION
+  let cloud = new aws.CloudWatchLogs({region})
+
+  let deleteLogs = streams => parallel(streams.map(log => cb => {
+    cloud.deleteLogStream({
+      logGroupName: name,
+      logStreamName: log.logStreamName,
+    }, cb)
+  }))
+
+  let count = 0
+
+  let run = () => {
+    cloud.describeLogStreams({logGroupName: name}, (err, streams) => {
+      streams = streams.logStreams
+      if (!streams.length) return callback(null, count)
+      count += streams.length
+      deleteLogs(streams, (err) => {
+        if (err) return callback(err)
+        else run()
+      })
+    })
+  }
+  run()
 }
