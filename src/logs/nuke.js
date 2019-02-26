d8a501d3d19862fd4994fab3e358649737801ac8
Mikeal Rogers <mikeal.rogers@gmail.com>
2019-02-25 21:01:58 -0800

    fix: missing callback

--- src/logs/nuke.js
+++ src/logs/nuke.js
@@ -6,20 +6,21 @@ module.exports = function nuke(name, callback) {
   let region = process.env.AWS_REGION
   let cloud = new aws.CloudWatchLogs({region})
 
-  let deleteLogs = streams => parallel(streams.map(log => cb => {
+  let deleteLogs = (streams, callback) => parallel(streams.map(log => cb => {
     cloud.deleteLogStream({
       logGroupName: name,
       logStreamName: log.logStreamName,
-    }, cb)
-  }))
+    }, cb 
+    )}), callback)
 
   let count = 0
 
   let run = () => {
     cloud.describeLogStreams({logGroupName: name}, (err, streams) => {
-      if (err) return callback(err)
       streams = streams.logStreams
-      if (!streams.length) return callback(null, count)
+      if (!streams.length) {
+        return callback(null, count)
+      }
       count += streams.length
       deleteLogs(streams, (err) => {
         if (err) return callback(err)
