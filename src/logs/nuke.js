c8e63f902bb551932775aa5a19016c8a84d533cb
Mikeal Rogers <mikeal.rogers@gmail.com>
2019-02-28 15:25:23 -0800

    wip: delete entire log group instead

--- src/logs/nuke.js
+++ src/logs/nuke.js
@@ -2,31 +2,8 @@ let aws = require('aws-sdk')
 let parallel = require('run-parallel')
 
 module.exports = function nuke(name, callback) {
-
   let region = process.env.AWS_REGION
   let cloud = new aws.CloudWatchLogs({region})
 
-  let deleteLogs = (streams, callback) => parallel(streams.map(log => cb => {
-    cloud.deleteLogStream({
-      logGroupName: name,
-      logStreamName: log.logStreamName,
-    }, cb 
-    )}), callback)
-
-  let count = 0
-
-  let run = () => {
-    cloud.describeLogStreams({logGroupName: name}, (err, streams) => {
-      streams = streams.logStreams
-      if (!streams.length) {
-        return callback(null, count)
-      }
-      count += streams.length
-      deleteLogs(streams, (err) => {
-        if (err) return callback(err)
-        else run()
-      })
-    })
-  }
-  run()
+  cloud.deleteLogGroup({logGroupName: name}, callback)
 }
