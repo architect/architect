99b3bca48ada02b7535fbd7e4b21abde872b05ae
Mike MacCana <mike@certsimple.com>
2019-04-08 17:00:13 +0100

    Fix 'http://localhost:undefined is already in use'. Fixes #95

--- src/sandbox/cli.js
+++ src/sandbox/cli.js
@@ -5,17 +5,20 @@ var start = require('./start')
 var canUse = require('./_port-in-use')
 var waterfall = require('run-waterfall')
 
+// use default from ./start module in case no port provided via env
+const PORT = process.env.PORT || 3333
+
 waterfall(
   [
-    canUse(process.env.PORT || 3333), // use default from ./start module in case no port provided via env
+    canUse(PORT),
     cleanEnv,
     start
   ],
   function done(err) {
     if (err) {
       console.log(chalk.bold.red(`Error`), chalk.white.bold(err.message))
-      if (err.code == 'EADDRINUSE') {
-        console.log(`The address http://localhost:${process.env.PORT} is already in use.`)
+      if (err.code == "EADDRINUSE") {
+        console.log(`The address http://localhost:${PORT} is already in use.`)
       }
       process.exit(1)
     }
