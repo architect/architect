f2563b0134da316d1a490fe0cc34107a37b939c3
Ryan Block <rblock@gmail.com>
2019-04-08 12:45:12 -0700

    Slightly better rate limit error message

--- src/create/errors/too-many-requests.js
+++ src/create/errors/too-many-requests.js
@@ -2,8 +2,9 @@ let chalk = require('chalk')
 
 module.exports = function tooManyRequests(err) {
   console.log(chalk.dim('-----'))
-  console.log(chalk.bold.red(`Error`), chalk.bold.white(err.message))
+  console.log(chalk.bold.red(`Rate limit error`), chalk.bold.white(err.message))
   console.log(chalk.dim('-----'))
-  console.log(`Congratulations: you have been throttled! This is a very common error with AWS and nothing to worry about. This means you are working faster than your account is currently provisioned to handle. Try re-running ` + chalk.cyan.bold('npx create') + ' after a few minutes. If the problem persists you can request limit increases by contacting AWS support.')
+  console.log(`We've hit rate limits provisioning your app. This is a common issue with provisioning certain AWS services, and is nothing to worry about. It means we're working faster than your account is currently provisioned to handle. Try re-running ` + chalk.cyan.bold('npx create') + ` in a few moments.`)
+  console.log(`If the problem persists, try using a different availability zone, or you can also request rate limit increases on some services by contacting AWS support.`)
   process.exit(1)
 }
