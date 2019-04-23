3aa54ce6f84a0aa2001eab65cf2ca19da00054ce
Brian LeRoux <b@brian.io>
2019-04-23 19:01:37 -0400

    dont show unneccessary stuff

--- src/inventory/nuke/websocketapis.js
+++ src/inventory/nuke/websocketapis.js
@@ -4,57 +4,62 @@ let waterfall = require('run-waterfall')
 
 module.exports = function snstopics(inventory, callback) {
 
-  let {header, notfound, error, deleted} = print(inventory)
+  if (inventory.types.ws.length === 0) {
+    callback()
+  }
+  else {
+    let {header, notfound, error, deleted} = print(inventory)
 
-  header('API Gateway WebSocket APIs')
+    header('API Gateway WebSocket APIs')
 
-  let gateway = new aws.ApiGatewayV2({region: process.env.AWS_REGION})
-  gateway.getApis({}, function getApis(err, result) {
-    if (err) error(err.message)
-    else {
+    let gateway = new aws.ApiGatewayV2({region: process.env.AWS_REGION})
+    gateway.getApis({}, function getApis(err, result) {
+      if (err) error(err.message)
+      else {
 
-      let tidy = i=> ({name: i.Name, id: i.ApiId})
-      let filter = t=> t.name.startsWith(inventory.app)
-      let apis = result.Items.map(tidy).filter(filter)
-      let staging = apis.find(o=> o.name === `${inventory.app}-ws-staging`)
-      let production = apis.find(o=> o.name === `${inventory.app}-ws-production`)
+        let tidy = i=> ({name: i.Name, id: i.ApiId})
+        let filter = t=> t.name.startsWith(inventory.app)
+        let apis = result.Items.map(tidy).filter(filter)
+        let staging = apis.find(o=> o.name === `${inventory.app}-ws-staging`)
+        let production = apis.find(o=> o.name === `${inventory.app}-ws-production`)
 
-      waterfall([
-        function deleteStaging(callback) {
-          if (!staging) {
-            notfound(`${inventory.app}-ws-staging`)
-            callback()
-          }
-          else {
-            gateway.deleteApi({
-              ApiId: staging.id
-            },
-            function deleteApi(err) {
-              if (err) error(err.message)
-              else deleted(staging.name, 'Deleted')
+        waterfall([
+          function deleteStaging(callback) {
+            if (!staging) {
+              notfound(`${inventory.app}-ws-staging`)
               callback()
-            })
-          }
-        },
-        function deleteProduction(callback) {
-          if (!production) {
-            notfound(`${inventory.app}-ws-production`)
-            callback()
-          }
-          else {
-            setTimeout(function delay() {
+            }
+            else {
               gateway.deleteApi({
-                ApiId: production.id
+                ApiId: staging.id
               },
               function deleteApi(err) {
                 if (err) error(err.message)
-                else deleted(production.name, 'Deleted')
+                else deleted(staging.name, 'Deleted')
                 callback()
               })
-            }, staging? 30*1000 : 0)
+            }
+          },
+          function deleteProduction(callback) {
+            if (!production) {
+              notfound(`${inventory.app}-ws-production`)
+              callback()
+            }
+            else {
+              setTimeout(function delay() {
+                gateway.deleteApi({
+                  ApiId: production.id
+                },
+                function deleteApi(err) {
+                  if (err) error(err.message)
+                  else deleted(production.name, 'Deleted')
+                  callback()
+                })
+              }, staging? 30*1000 : 0)
+            }
           }
-        }
-      ], callback)
-    }
-  })
+        ], callback)
+      }
+    })
+  }
 }
