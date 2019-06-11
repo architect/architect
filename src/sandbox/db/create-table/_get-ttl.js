ccb9e8dd5985618385c8b170449b47844e41d0ed
Ryan Block <rblock@gmail.com>
2019-06-11 15:39:39 -0700

    First pass at integrating sandbox module into Architect

--- src/sandbox/db/create-table/_get-ttl.js
+++
@@ -1,9 +0,0 @@
-module.exports = function getTTL(attr) {
-  var found = false
-  Object.keys(attr).forEach(k=> {
-    if (attr[k] === 'TTL') {
-      found = k
-    }
-  })
-  return found
-}
--- src/sandbox/index.js
+++ src/sandbox/index.js
@@ -1,13 +1,16 @@
-let db = require('./db')
-let env = require('./env')
-let events = require('./events')
-let http = require('./http')
-let start = require('./start')
+let sandbox = require('@architect/sandbox')
+let version = require('../../package.json').version
 
-module.exports = {
-  db,
-  env,
-  events,
-  http,
-  start,
+module.exports = function sandboxImplementation(opts) {
+  process.env.ARC_VERSION = `Architect ${version}`
+  let findPort = opt=> opt === '-p' || opt === '--port' || opt === 'port'
+  if (opts && opts.some(findPort)) {
+    if (opts.indexOf('-p') >= 0)
+      process.env.PORT = opts[opts.indexOf('-p') + 1]
+    if (opts.indexOf('--port') >= 0)
+      process.env.PORT = opts[opts.indexOf('--port') + 1]
+    if (opts.indexOf('port') >= 0)
+      process.env.PORT = opts[opts.indexOf('port') + 1]
+  }
+  sandbox.start()
 }
