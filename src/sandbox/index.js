a9c79406764725620405da1f22be5fe022db07e9
Ryan Block <rblock@gmail.com>
2019-06-12 15:31:54 -0700

    Passes sandbox version via param, updates deps

--- src/sandbox/index.js
+++ src/sandbox/index.js
@@ -1,8 +1,8 @@
 let sandbox = require('@architect/sandbox')
-let version = require('../../package.json').version
+let ver = require('../../package.json').version
 
 module.exports = function sandboxImplementation(opts) {
-  process.env.ARC_VERSION = `Architect ${version}`
+  let version = `Architect ${ver}`
   let findPort = opt=> opt === '-p' || opt === '--port' || opt === 'port'
   if (opts && opts.some(findPort)) {
     if (opts.indexOf('-p') >= 0)
@@ -12,5 +12,5 @@ module.exports = function sandboxImplementation(opts) {
     if (opts.indexOf('port') >= 0)
       process.env.PORT = opts[opts.indexOf('port') + 1]
   }
-  sandbox.start()
+  sandbox.start({version})
 }
