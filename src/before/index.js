99002091c59fab2b01a07786b8c15e9b066d8bf3
Ryan Block <rblock@gmail.com>
2019-10-19 13:26:19 -0700

    Fixes env (and possibly other commands) sending bootstrap unnecessary args

--- src/before/index.js
+++ src/before/index.js
@@ -3,8 +3,7 @@ let ensureCreds = require('./01-ensure-creds')
 let maybeBanner = require('./02-maybe-banner')
 
 module.exports = function before() {
-  let options = process.argv
-  bootstrap({options})  // maybe create .arc
-  ensureCreds()         // loads AWS_REGION and AWS_PROFILE
-  maybeBanner()         // maybe print cli banner
+  bootstrap({}) // maybe create .arc
+  ensureCreds() // loads AWS_REGION and AWS_PROFILE
+  maybeBanner() // maybe print cli banner
 }
