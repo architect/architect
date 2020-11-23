cd500df8e34ac5d91118c3ead996575bd2e7433e
Ryan Block <rblock@gmail.com>
2020-11-22 18:09:57 -0800

    Disable bootstrapping, Inventory can backfill default project, let modules handle auto-create

--- src/before/02-banner.js
+++ src/before/02-banner.js
@@ -1,21 +1,15 @@
-let { banner, updater } = require('@architect/utils')
+let { banner } = require('@architect/utils')
 let { version: ver } = require('../../package.json')
 
 module.exports = function runBanner ({ cmd, inventory }) {
   try {
     // Commands specified below musthave valid credetials to operate
-    let needsValidCreds = cmd == 'deploy' ||
-                          cmd == 'env' ||
-                          cmd == 'logs'
+    let needsValidCreds = [ 'deploy', 'env', 'logs' ].includes(cmd)
     banner({
       inventory,
       needsValidCreds,
       version: `Architect ${ver}`
     })
-    if (process.env.INITIALIZED) {
-      let update = updater('Create')
-      update.done('Created Architect project manifest (.arc)')
-    }
   }
   catch (e) {
     console.log(e)
--- src/before/index.js
+++ src/before/index.js
@@ -1,9 +1,7 @@
-let { bootstrap } = require('@architect/create')
 let basicEnv = require('./01-env')
 let banner = require('./02-banner')
 
 module.exports = function before (params) {
-  bootstrap(params) // Maybe create new project files
   basicEnv()  // Load essential env vars (NODE_ENV, AWS_REGION, etc.)
   banner(params) // Print banner + initialize AWS credentials
 }
