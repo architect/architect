be8be096dffc4773b7d11d42015ba3d8f86f9bf0
Ryan Block <rblock@gmail.com>
2022-01-22 10:18:00 -0800

    Breaking change: no longer use `NODE_ENV`

--- src/startup/env.js
+++ src/startup/env.js
@@ -2,12 +2,10 @@
  * Ensures the following env vars are present:
  *
  * - ARC_ENV (default 'testing')
- * - NODE_ENV (default 'testing'; possibly munged by 3rd party libraries/frameworks)
  * - AWS_REGION (default us-west-2)
  */
 module.exports = function ensureEnv () {
-  // always ensure ARC_ENV + NODE_ENV + AWS_REGION
+  // always ensure ARC_ENV + AWS_REGION
   if (!process.env.ARC_ENV) process.env.ARC_ENV = 'testing'
-  if (!process.env.NODE_ENV) process.env.NODE_ENV = 'testing'
   if (!process.env.AWS_REGION) process.env.AWS_REGION = 'us-west-2'
 }
