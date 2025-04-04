ce62c0cf3cf5f5fa77523fcccfc1528a72a9c7af
Fil Maj <maj.fil@gmail.com>
2025-04-03 23:39:56 -0400

    docs: update CLI help text (#1507)
    * docs: update create/init help text
    
    * docs: update deploy cli help text
    
    * docs: update destroy cli help text
    
    * docs: update env cli help text
    
    * docs: update sandbox cli help text
    
    * docs: update logs cli help text

--- src/help/index.js
+++ src/help/index.js
@@ -9,10 +9,11 @@ let helps = {
   help: `${G('arc [command] <options>')}
 
 ${D('Global Commands')}
-  ${g('arc', G('<init|create>'), '[name or path]')} ${d('.......... initialize project files')}
+  ${g('arc', G('<init|create>'), '[name or path]')} ${d('.......... initialize new arc project')}
   ${g('arc', G('help'), '<command>')} ${d('........................ get help')}
   ${g('arc', G('version'))} ${d('............................... get the current version')}
 ${D('Project Commands')}
+  ${g('arc', G('<init|create>'))} ${d('......................... idempotently initialize project files')}
   ${g('arc', G('sandbox'))} ${d('............................... start a local arc development server')}
   ${g('arc', G('deploy'), '[direct|static|production]')} ${d('..... deploy to AWS')}
   ${g('arc', G('logs'), 'path/to/fn', '[production|destroy]')} ${d('.. manage function logs')}
@@ -20,82 +21,134 @@ ${D('Project Commands')}
   ${g('arc', G('destroy'))} ${d('............................... destroy your current project')}
 `,
 
-  init: `${G('arc <init|create>')} ${g('[app-name or path] [options]')}
-Generate an arc project.
-When ${b('no arguments are passed')}, arc will look for an app.arc file in the local directory and ensure any resources defined in it are created locally.
+  init: `${G('arc <init|create>')} ${g('[name or path] [options]')}
+Generate a new arc project at the specified path, or if run in an existing arc project, idempotently initialize any missing function directories.
+When used with the ${g('--plugin')} flag, generates a new arc plugin project at the specified path instead.
+When ${b('no arguments are passed')}, arc will look for an app.arc file in the local directory and idempotently initialize any resources defined in it; that is, if they do not exist, they will be created locally.
 When passed a ${b('path-like argument')}, arc will create a new project in the specified path (including an app.arc project manifest file).
 When passed ${b('any other argument')}, arc will create a new project with that argument as the app name in the current working directory (including an app.arc project manifest file).
 
 ${D('Options')}
-  ${g(`-s${d(',')}`, `--static${d(',')}`)} ${d('........... initialize a static web app')}
-  ${g(`-r${d(',')}`, `--runtime${d(',')}`)} ${d('......... initialize project with a specific runtime; can be one of:')}
-    ${d(D('node'), ',', D('js'), '..... nodejs14.x')}
+  ${g(`-n${d(',')}`, '--name')} ${d('............ app name; sets `@app` in your app.arc file')}
+  ${g('--no-install')} ${d('.......... do not automatically install `@architect/architect` as a dependency in the project')}
+  ${g(`-p${d(',')}`)} ${g('--plugin')} ${d('.......... create a scaffolded architect plugin project')}
+  ${g(`-r${d(',')}`, `--runtime ${d('......... initialize project with a specific runtime. If unspecified, will default to node. Can be one of:')}`)}
+    ${d(D('node'), ',', D('js'), '..... nodejs22.x')}
     ${d(D('deno'), '.......... deno')}
-    ${d(D('rb'), ',', D('ruby'), '..... ruby2.7')}
-    ${d(D('python'), ',', D('py'), '... python3.9')}`
+    ${d(D('rb'), ',', D('ruby'), '..... ruby3.3')}
+    ${d(D('python'), ',', D('py'), '... python3.13')}
+  ${g(`-v${d(',')}`)} ${g('--verbose')} ${d('......... run in verbose mode')}`
   ,
 
   deploy: `${G('arc deploy')} ${g('[options]')}
-Deploy with ${b('AWS SAM')} to a CloudFormation Stack name of the form AppNameEnvironment (default environment is staging)
+Deploy your app, as defined in your app.arc manifest, to a CloudFormation Stack. The @app name and environment (more on this below) determine the Stack your application will deploy to. Furthermore, Stacks are unique per region therefore changing your target AWS region will change the target Stack to deploy to.
+By default, this command deploys a staging version of your application to a dedicated CloudFormation Stack. By providing the ${g('-p')} or ${g('--production')} flag you can deploy to a separate production Stack.
+You can target additional Stacks by using the ${g('--name')} flag; this will append the provided name to the Stack name and is a technique to create arbitrary additional deployment environments beyond staging and production.
+When passed the ${g('--direct')} flag in conjunction with a path-like argument to a Lambda source directory, instead of a full CloudFormation deploy, arc will only deploy source code for the provided Lambda. This will be much faster and may be helpful when wanting to deploy small, isolated changes.
 
 ${D('Options')}
-  ${g(`-p${d(',')}`, `--production${d(',')}`)} ${d(`............ set environment to production, i.e. ${D('--production')} results in a Stack name of ${D('AppNameProduction')}`)}
-  ${g(`--direct${d(',')}`, '<path/to/function>')} ${d('... directly deploy only function code and config by uploading and overwriting; optionally specify a path to one or more functions to only deploy the specified functions')}
-  ${g(`-s${d(',')}`, `--static${d(',')}`)} ${d('.................... upload static assets (by default in /public) to the static S3 bucket')}
-  ${g(`-v${d(',')}`, `--verbose${d(',')}`)} ${d('.................. prints all output to console')}
-  ${g(`-n${d(',')}`, `--name${d(',')}`)} ${d(`........................ append to Stack name, i.e. ${D('--name CI')} results in a Stack name of ${D('AppNameStagingCI')}`)}
-  ${g(`-t${d(',')}`, `--tags${d(',')}`)} ${d('........................ add tags to Stack')}
-  ${g(`--prune${d(',')}`)} ${d('.......................... delete orphaned static files from the static S3 bucket; files that are not present in the local @static directory (by default in /public) will be removed')}
-  ${g('--no-hydrate')} ${d('............................ skip hydrating functions before deploy; arc will not try to install dependencies in each of your function directories')}
+  ${g(`--direct`, '<path/to/function>')} ${d('... directly deploy only provided function code and config by uploading and overwriting; optionally specify a path to one or more functions to only deploy the specified functions')}
+  ${g('--dry-run')} ${d('..................... generate a CloudFormation template but do not deploy it; handy to validate CloudFormation and SAM output or to test Architect plugins')}
+  ${g('--eject')} ${d('....................... generate a CloudFormation template but do not deploy it and print the `aws cloudformation` command to run to deploy it')}
+  ${g(`-f${d(',')}`, `--fast`)} ${d('.................... deploy the Stack but do not wait until deployment completes')}
+  ${g(`-n${d(',')}`, `--name`)} ${d(`.................... append to Stack name, i.e. ${D('--name CI')} results in a Stack name of ${D('AppNameStagingCI')}`)}
+  ${g('--no-hydrate')} ${d('.................. skip hydrating functions before deploy; arc will not try to install dependencies in each of your function directories')}
+  ${g(`-p${d(',')}`, `--production`)} ${d(`.............. set environment to production, i.e. ${D('--production')} results in a Stack name of ${D('AppNameProduction')}`)}
+  ${g(`--prune`)} ${d('....................... delete orphaned static files from the static S3 bucket; files that are not present in the local @static directory (by default in /public) will be removed')}
+  ${g(`-s${d(',')}`, `--static`)} ${d('.................. only upload static assets (by default in /public) to the static S3 bucket')}
+  ${g(`-t${d(',')}`, `--tags`)} ${d('.................... add tags to Stack')}
+  ${g(`-v${d(',')}`, `--verbose`)} ${d('................. print more output to console')}
+  ${g(`-d${d(',')}`, `--debug`)} ${d('................... print even more output to console')}
 `,
 
   sandbox: `${G('arc sandbox')} ${g('[options]')}
-Start a local web server and in-memory database. This server will mount your @http and @ws functions on the routes you have defined in your app.arc. It will also serve your @static assets. @events and @queues are supported. @tables and @indexes will be created using Dynalite, a fast in-memory database with a DynamoDB API.
+Start a local development server that emulates AWS infrastructure for your Architect application. Sandbox provides a complete local development environment that includes:
+
+- HTTP server for your @http routes
+- WebSocket server for @ws routes
+- Dynalite in-memory database for @tables and @indexes
+- Local event bus for @events and @queues
+- Static asset serving for @static assets
+- File watching and live reloading
 
 ${D('Options')}
-  ${g(`-p${d(',')}`, `--port${d(',')}`)} ${d('..... port the web server will listen on (default is 3333)')}
-  ${g(`--disable-symlinks`)} ${d('... do not use symlinks to mount `src/shared` into all arc functions; instead copy files direct (warning: slower)')}
+  ${g(`-p${d(',')} --port`)} ${d('........................ port the HTTP server will listen on (default is 3333)')}
+  ${g(`-h${d(',')} --host`)} ${d('........................ host the server will bind to')}
+  ${g(`--disable-symlinks`)} ${d('................ do not use symlinks for shared code; use file copying instead (slower)')}
+  ${g(`--disable-delete-vendor`)} ${d('........... do not delete node_modules or vendor directories upon startup')}
+  ${g(`-q${d(',')} --quiet`)} ${d('....................... minimize console output during operation')}
+  ${g(`-v${d(',')} --verbose`)} ${d('..................... print more detailed output during operation')}
+  ${g(`-d${d(',')} --debug`)} ${d('....................... print even more detailed information for debugging')}
+
+${D('Environment Variables')}
+  ${d(`${g('ARC_HTTP_PORT')}, ${g('PORT')} ............... set the HTTP server port (same as --port)`)}
+  ${d(`${g('ARC_EVENTS_PORT')} ................... set the events/queues service port (default 4444)`)}
+  ${d(`${g('ARC_TABLES_PORT')} ................... set the DynamoDB emulator port (default 5555)`)}
+  ${d(`${g('ARC_HOST')} .......................... set the host the server will bind to`)}
+  ${d(`${g('ARC_QUIET')}, ${g('QUIET')} .................. minimize console output (same as --quiet)`)}
 
 ${D('Keyboard Shortcuts')}
-${d('Sandbox registers a few keyboard shortcuts you may invoke to help with local development (note: they are all capital letters!):')}
-  ${g('S')} ${d('..... re-hydrates (installs dependencies) `src/shared` and copies or symlinks it into all function code')}
-  ${g('V')} ${d('..... re-hydrates (installs dependencies) `src/views` and copies or symlinks it into all function code')}
-  ${g('H')} ${d('..... re-hydrates (installs dependencies) both `src/shared` and `src/views` and copies or symlinks both into all function code')}
+${d('Sandbox registers keyboard shortcuts to help with local development (note: they are all capital letters!):')}
+  ${g('S')} ${d('................................. rehydrate only src/shared')}
+  ${g('V')} ${d('................................. rehydrate only src/views')}
+  ${g('H')} ${d('................................. rehydrate both src/shared and src/views')}
+  ${g('Ctrl+C')} ${d('............................ gracefully shut down the sandbox')}
 `,
 
   version: `${G('arc version')}
 Print the current version.`,
 
-  env: `${G('arc env [[--add|--remove] --env <testing|staging|prduction> VARIABLE_NAME variable_value]')}
-Read and write environment variables. Sensitive configuration data, such as API keys, needs to happen outside of revision control and you can use this tool to ensure an entire team and the deployment targets are in sync.
-Any environment variables written using this command will be available to functions at runtime. ${b('The \'testing\' environment is for local execution using `arc sandbox`')}.
-By default, environment variables will be written to the preferences.arc file. However, if a .env file exists in the project root, variables will be written there instead.
+  env: `${G('arc env')} ${g('[options]')}
+Manage environment variables for your Architect application across different environments. This command allows you to read, add, and remove environment variables that will be available to your Lambda functions at runtime.
+When run ${b('without any flags')}, this command will print out all environment variables and their values, across all environments.
+Environment variables are stored in AWS SSM Parameter Store for staging and production environments. For local development (testing environment), variables are stored in your project's preferences.arc file or .env file if one exists.
+${b('The \'testing\' environment is used for local execution with `arc sandbox`')}. Variables for this environment are stored locally, while staging and production variables are stored in AWS SSM.
 
 ${D('Options')}
-${g(`arc env`)} ${d('..................................................................... displays all environment variables for the current app.arc and writes them to prefs.arc or .env')}
-  ${g(`arc env [-e|--env [testing|staging|production]]`)} ${d('........................................ displays all environment variables for the specified environment')}
-  ${g(`arc env <-a|--add> -e <testing|staging|production> VARIABLE_NAME variable_value`)} ${d('................ assigns a value to the specified variable name in the specified environment')}
-  ${g(`arc env <-r|--remove> -e <testing|staging|production> VARIABLE_NAME`)} ${d('... removes the specified variable from the specified environment')}
+  ${g(`-e${d(',')} --env <environment>`)} ${d('... specify environment (testing, staging, or production)')}
+  ${g(`-a${d(',')} --add`)} ${d('................. add a new environment variable')}
+  ${g(`-r${d(',')} --remove`)} ${d('.............. remove an environment variable')}
+  ${g(`-v${d(',')} --verbose`)} ${d('............. print more detailed output')}
+  ${g(`-d${d(',')} --debug`)} ${d('............... print even more detailed information for debugging')}
+
+${D('Examples')}
+  ${g(`arc env`)} ${d('......................................... display all environment variables for all environments')}
+  ${g(`arc env --env staging`)} ${d('........................... display all environment variables for staging')}
+  ${g(`arc env --add --env testing MY_VAR "my value"`)} ${d('... add MY_VAR to testing environment')}
+  ${g(`arc env --remove --env production DB_PASS`)} ${d('....... remove DB_PASS from production environment')}
 `,
 
-  destroy: `${G('arc destroy')}
-Destroy your Architect application. Deletes all resources associated with your app (including CloudFormation Stack, environment variables, CloudWatch Logs and deployment bucket). Unless --production or --name are specified, this command will destroy your staging Stack.
+  destroy: `${G('arc destroy')} ${g('[options]')}
+Completely removes your Architect application from AWS. This command deletes all resources associated with your application, including the CloudFormation Stack, SSM environment variables, CloudWatch Logs, deployment S3 bucket, and static S3 bucket (if applicable). 
+By default, this command destroys your staging environment. Use the ${g('--production')} flag to target the production environment instead. For custom environments created with ${g('deploy --name')}, use the ${g('--name')} flag to specify the environment to destroy.
+For safety reasons, if your application has database tables or static assets, ${b('you must explicitly confirm deletion')} by using the ${g('--force')} flag. There is a 5-second safety delay before destruction begins, which can be bypassed with the ${g('--now')} flag.
 
 ${D('Options')}
-  ${g(`--app`)} ${d('.......... name of your app (required)')}
-  ${g(`--name`)} ${d('......... target custom named environment; used to destroy an app that used `deploy`\'s --name flag')}
-  ${g(`--force`)} ${d('........ destroy an app that has database tables and/or static assets')}
-  ${g(`--production`)} ${d('... destroy the production version of your app')}
-  ${g(`--no-timeout`)} ${d('... by default, `destroy` times out after approximately 150 seconds, but by specifying this flag, `destroy` will wait until all application resources are removed before exiting')}
+  ${g(`--app`)} ${d('............. app name (required for confirmation)')}
+  ${g(`--force${d(',')} -f`)} ${d('....... destroy an app that has database tables and/or static assets')}
+  ${g(`--name`)} ${d('............ target a custom named environment created with `deploy --name`')}
+  ${g(`--now`)} ${d('............. skip the 5-second safety delay before destroying resources')}
+  ${g(`--no-timeout`)} ${d('...... wait indefinitely for all application resources to be removed (by default times out after ~150 seconds)')}
+  ${g(`--production${d(',')} -p`)} ${d('.. destroy the production environment instead of staging')}
+  ${g(`--verbose${d(',')} -v`)} ${d('..... print more detailed information during the destroy process')}
+  ${g(`--debug${d(',')} -d`)} ${d('....... print even more detailed information for debugging purposes')}
 `,
 
-  logs: `${G('arc logs [options] <path/to/function>')}
-Anytime your remotely-deployed functions log to stdout, those get stored in CloudWatch. \`arc logs\` lets you retrieve these logs for debugging or monitoring. ${b('You must provide a path argument to one of your functions')} (i.e. \`arc logs src/http/get-index\`). Unless --production is specified, this command will retrieve logs from the staging Stack.
+  logs: `${G('arc logs')} ${g('[options] <path/to/function>')}
+Retrieve or delete CloudWatch logs for your Lambda functions. When your cloud functions log to stdout or stderr, these messages are stored in AWS CloudWatch. This command lets you access these logs.
+${b('You must provide a path to one of your Lambda function directories')} (e.g., ${g('arc logs src/http/get-index')}). By default, this command retrieves logs from your staging environment, unless the ${g('--production')} flag is specified.
+If no function path is provided and your project has a root HTTP handler defined, logs for that handler will be retrieved by default.
 
 ${D('Options')}
-  ${d(`${g('-v')}, ${g(`--verbose`)}, ............... run in verbose mode`)}
-  ${d(`${g('-n')}, ${g(`--destroy`)}, ............... delete logs for the specified function`)}
-  ${d(`${g('-p')}, ${g(`--production`)}, ............ retrieve (or delete, if combined with --destroy) logs for the production Stack`)}
+  ${g(`--destroy`)} ${d('........................ delete logs for the specified function')}
+  ${g(`-p${d(',')} --production`)} ${d('................. retrieve logs from the production environment (default is staging)')}
+  ${g(`-v${d(',')} --verbose`)} ${d('.................... display more detailed information during command execution')}
+  ${g(`-d${d(',')} --debug`)} ${d('...................... display even more detailed information for debugging purposes')}
+
+${D('Examples')}
+  ${g(`arc logs src/http/get-index`)} ${d('................ retrieve logs for the index function from staging')}
+  ${g(`arc logs --production src/http/get-index`)} ${d('... retrieve logs for the index function from production')}
+  ${g(`arc logs --destroy src/events/send-email`)} ${d('... delete logs for the send-email function in staging')}
 `,
 
 }
